ce1b095c073924179f9862ce9b0bad9b3f617c8c
Szymon Rogowski <szymon0r0official@gmail.com>
2023-09-12 15:50:26 +0200

    pulling cluster folder variables and stuff

--- main.tf
+++ main.tf
@@ -17,3 +17,12 @@ provider "azurerm" {
   features {}
 }
 
+module "cluster" {
+  source = "./cluster"
+  serviceprinciple_id = var.serviceprinciple_id
+  serviceprinciple_key = var.serviceprinciple_key
+  ssh_key = var.ssh_key
+  location = var.location
+  kubernetes_version = var.kubernetes_version
+}
+
